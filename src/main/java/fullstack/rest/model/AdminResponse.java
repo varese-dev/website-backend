7e0a9149d1ed3750ab669094222f389069596d87
Mattia Mossali <mossalimattia@gmail.com>
2025-02-06 12:38:35 +0100

    adminResponse fix

--- src/main/java/fullstack/rest/model/AdminResponse.java
+++ src/main/java/fullstack/rest/model/AdminResponse.java
@@ -1,18 +1,11 @@
 package fullstack.rest.model;
 
+import fullstack.persistence.model.Role;
+
 public class AdminResponse extends UserResponse {
     private String role;
 
-    public AdminResponse(String name, String surname, String email, String phone, String role) {
-        super(name, surname, email, phone);
-        this.role = role;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
+    public AdminResponse(String name, String surname, String email, String phone, Role role) {
+        super(name, surname, email, phone, role);
     }
 }
